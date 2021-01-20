c63d6b1eee6908ac10595767ceaea90638ff2751
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2021-01-20 08:22:44 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export { default as Button } from './components/Button/Button';
 export { default as IconButton } from './components/IconButton/IconButton';
-// export { default as Checkbox } from './components/Checkbox/Checkbox';
+export { default as TestComponent } from './components/TestComponent/TestComponent';
