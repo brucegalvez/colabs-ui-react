0b804b74103e22c397df2622d57e1a9b1b86209f
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2021-03-29 08:32:03 -0500

    asdasd

--- src/utils/CssBaseline/CssBaseline.jsx
+++ src/utils/CssBaseline/CssBaseline.jsx
@@ -4,33 +4,33 @@ import './normalize.css';
 const CssBaseline = createGlobalStyle`
   @font-face {
     font-family: 'TTNormsRegular';
-    src: local('TTNorms Regular'), url(../../../assets/fonts/TT-Norms/TTNorms-Regular.woff2) format('woff2');
+    src: local('TTNorms Regular'), url(./src/assets/fonts/TT-Norms/TTNorms-Regular.woff2) format('woff2');
   }
   @font-face {
     font-family: 'TTNormsMedium';
-    src: local('TTNorms Medium'), url(../../../assets/fonts/TT-Norms/TTNorms-Medium.woff2) format('woff2');
+    src: local('TTNorms Medium'), url(./src/assets/fonts/TT-Norms/TTNorms-Medium.woff2) format('woff2');
   }
   @font-face {
     font-family: 'TTNormsLight';
-    src: local('TTNorms Light'), url(../../../assets/fonts/TT-Norms/TTNorms-Light.woff2) format('woff2');
+    src: local('TTNorms Light'), url(./src/assets/fonts/TT-Norms/TTNorms-Light.woff2) format('woff2');
   }
   @font-face {
     font-family: 'TTNormsBlack';
-    src: local('TTNorms Black'), url(../../../assets/fonts/TT-Norms/TTNorms-Black.woff2) format('woff2');
+    src: local('TTNorms Black'), url(./src/assets/fonts/TT-Norms/TTNorms-Black.woff2) format('woff2');
   }
   @font-face {
     font-family: 'TTNormsBold';
-    src: local('TTNorms Bold'), url(../../../assets/fonts/TT-Norms/TTNorms-Bold.woff2) format('woff2');
+    src: local('TTNorms Bold'), url(./src/assets/fonts/TT-Norms/TTNorms-Bold.woff2) format('woff2');
     font-style: normal;
   }
   @font-face {
     font-family: 'TTNormsItalic';
-    src: local('TTNorms Italic'), url(../../../assets/fonts/TT-Norms/TTNorms-Italic.woff2) format('woff2');
+    src: local('TTNorms Italic'), url(./src/assets/fonts/TT-Norms/TTNorms-Italic.woff2) format('woff2');
     font-style: normal;
   }
   @font-face {
     font-family: 'TTNormsMediumItalic';
-    src: local('TTNorms Medium Italic'), url(../../../assets/fonts/TT-Norms/TTNorms-MediumItalic.woff2) format('woff2');
+    src: local('TTNorms Medium Italic'), url(./src/assets/fonts/TT-Norms/TTNorms-MediumItalic.woff2) format('woff2');
     font-style: normal;
   }
 * {
